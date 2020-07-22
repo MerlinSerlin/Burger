ccac164c3e8e9baa419ab534317e52e306a0066f
Marshall Serlin <Marshallserlin@gmail.com>
2020-07-22 11:03:41 -0700

    before sidedrawer toggle

---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import classes from './Toolbar.module.css'
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationsItems';
+
+const toolbar = (props) => (
+    <header className={classes.Toolbar}>
+        <div>MENU</div>
+        <div className={classes.Logo}>
+            <Logo />
+        </div>
+        <nav className={classes.DesktopOnly}>
+            <NavigationItems></NavigationItems>
+        </nav>
+    </header>
+)
+
+
+export default toolbar;
