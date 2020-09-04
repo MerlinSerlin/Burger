df63daa6342fc11def9b09a2a73fb71121667c64
Marshall Serlin <Marshallserlin@gmail.com>
2020-09-03 21:04:13 -0700

    jest and enzyme

---
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -0,0 +1,18 @@
+const { initIngredients } = require("../../../store/actions");
+
+import React from 'react';
+
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import NavigationItems from './NavigationsItems';
+import NavigationItem from './NavigationItem/NavigationItem';
+
+configure({adapter: new Adapter()});
+
+describe('<NavigationItems />', () => {
+    it('should render 2 <NavigationItem > elements if not authenticated', () => {
+        const wrapper = shallow(<NavigationItems/>);
+        expect(wrapper.find(NavigationItem)).toHaveLength(2);
+    });
+});
