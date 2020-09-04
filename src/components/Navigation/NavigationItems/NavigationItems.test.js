0f3fa9defb4babdf0bbea32cf6498ac63cce087d
Marshall Serlin <Marshallserlin@gmail.com>
2020-09-04 11:55:09 -0700

    set up first tests

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -11,8 +11,23 @@ import NavigationItem from './NavigationItem/NavigationItem';
 configure({adapter: new Adapter()});
 
 describe('<NavigationItems />', () => {
+    let wrapper;
+    
+    beforeEach(() => {
+        wrapper = shallow(<NavigationItems/>);
+    })
+
     it('should render 2 <NavigationItem > elements if not authenticated', () => {
-        const wrapper = shallow(<NavigationItems/>);
         expect(wrapper.find(NavigationItem)).toHaveLength(2);
+    });
+
+    it('should render 3 <NavigationItem > elements if  authenticated', () => {
+        wrapper.setProps({isAuthenticated: true})
+        expect(wrapper.find(NavigationItem)).toHaveLength(3);
+    });
+
+    it('should render <NavigationItem > if  authenticated', () => {
+        wrapper.setProps({isAuthenticated: true})
+        expect(wrapper.contains(<NavigationItem link="/logout">Log Out</NavigationItem>)).toEqual(true);
     });
 });
